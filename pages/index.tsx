5657ac110fdce61a5243ef933f854d311b9fd0ed
SeongMin Han <hsm950825@naver.com>
2021-04-27 00:13:01 +0900

    Add : 기본 페이지 생성

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-size: 21px;
+  color: gray;
+`;
+
+const index: React.FC = () => {
+  return <Container>Hello World</Container>;
+};
+
+export default index;
