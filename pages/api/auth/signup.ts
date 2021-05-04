d49781901a173415202e7f9492fcd0b1bcee362f
SeongMin Han <hsm950825@naver.com>
2021-05-05 02:46:47 +0900

    Update : Signup API Logics

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,26 +1,66 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import Data from "../../../lib/data";
+import { StoredUserType } from "types/user";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // 계정 생성하기
   if (req.method === "POST") {
-    const { email, firstname, lastname, password, birthday } = body;
+    const { email, firstname, lastname, password, birthday } = req.body;
+
     if (!email || !firstname || !lastname || !password || !birthday) {
       res.statusCode = 400;
       return res.send("필수 데이터가 없습니다.");
     }
 
+    const userExist = Data.user.exist({ email });
+
+    if (userExist) {
+      res.statusCode = 409;
+      res.send("이미 가입된 이메일입니다.");
+    }
+
     const hashedPassword = bcrypt.hashSync(password, 8);
-    return res.end();
+
+    const users = Data.user.getList();
+    let userId;
+
+    if (users.length === 0) {
+      userId = 1;
+    } else {
+      userId = users[users.length - 1].id + 1;
+    }
+
+    const newUser: StoredUserType = {
+      id: userId,
+      email,
+      firstname,
+      lastname,
+      password: hashedPassword,
+      birthday,
+      profileImage: "/static/image/user/default_user_profile_image.jpg",
+    };
+
+    Data.user.write([...users, newUser]);
+
+    const token = jwt.sign(String(newUser.id), process.env.JWT_SECRET!);
+    res.setHeader(
+      "set-Cookie",
+      `access_token=${token}; path=/; expires=${new Date(
+        Date.now() + 60 * 60 * 24 * 1000 * 3 //3일
+      )}; httponly`
+    );
+
+    const newUserWithoutPassword: Partial<
+      Pick<StoredUserType, "password">
+    > = newUser;
+
+    delete newUserWithoutPassword.password;
+    res.statusCode = 200;
+    return res.send(newUser);
   }
 
-  const userExist = Data.user.exist({ email });
-
-  if (userExist) {
-    res.statusCode = 409;
-    res.send("이미 가입된 이메일입니다.");
-  }
   res.statusCode = 405;
 
   return res.end();
