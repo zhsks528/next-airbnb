488165312da692db5aaa3abdf8ec439f12746558
SeongMin Han <hsm950825@naver.com>
2021-05-04 12:51:59 +0900

    Update : SignUpModal Input Logic && HidePassword

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CloseXIcon from "../../public/static/svg/modal/modal_close_x_icon.svg";
 import MailIcon from "../../public/static/svg/auth/mail.svg";
 import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
+import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
 import Input from "../common/Input";
 
 const Container = styled.div`
@@ -26,6 +27,37 @@ const InputWrapper = styled.div`
 `;
 
 const SignUpModal: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [password, setPassword] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+
+  // 이메일 주소 변경 함수
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  // 이름 변경 함수
+  const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastname(event.target.value);
+  };
+
+  // 성 변경 함수
+  const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstname(event.target.value);
+  };
+
+  // 비밀번호 변경 함수
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  // 비밀번호 숨김 토글 함수
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
   return (
     <Container>
       <ModalCloseXIcon />
@@ -35,19 +67,39 @@ const SignUpModal: React.FC = () => {
           type="email"
           name="email"
           icon={<MailIcon />}
+          value={email}
+          onChange={onChangeEmail}
         />
       </InputWrapper>
       <InputWrapper>
-        <Input placeholder="이름(예: 길동)" icon={<PersonIcon />} />
+        <Input
+          placeholder="이름(예: 길동)"
+          icon={<PersonIcon />}
+          value={lastname}
+          onChange={onChangeLastname}
+        />
       </InputWrapper>
       <InputWrapper>
-        <Input placeholder="성(예: 홍)" icon={<PersonIcon />} />
+        <Input
+          placeholder="성(예: 홍)"
+          icon={<PersonIcon />}
+          value={firstname}
+          onChange={onChangeFirstname}
+        />
       </InputWrapper>
       <InputWrapper>
         <Input
           placeholder="비밀번호 설정하기"
-          type="password"
-          icon={<OpenedEyeIcon />}
+          type={hidePassword ? "password" : "text"}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
         />
       </InputWrapper>
     </Container>
